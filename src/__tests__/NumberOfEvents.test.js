ec815f95ee276cac02817174427326b2627b4228
Ian Baumeister <ian@avotravel.com>
2024-09-11 18:27:13 +0200

    Added defaultNumber and expectedNumber instead of hardcoded values

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -5,6 +5,7 @@ import NumberOfEvents from '../components/NumberOfEvents';
 describe('<NumberOfEvents /> component', () => {
   let NumberOfEventsComponent;
   const setCurrentNOE = jest.fn();
+  const defaultNumber = 32;
 
   beforeEach(() => {
     NumberOfEventsComponent = render(<NumberOfEvents setCurrentNOE={setCurrentNOE} />);
@@ -17,18 +18,20 @@ describe('<NumberOfEvents /> component', () => {
 
   test('default value of the input is 32', () => {
     const numberInput = NumberOfEventsComponent.getByRole('spinbutton');
-    expect(numberInput).toHaveValue(32);
+    expect(numberInput).toHaveValue(defaultNumber);
   });
 
   test('value changes when user types in the input', () => {
     const numberInput = screen.getByRole('spinbutton');
-    fireEvent.change(numberInput, { target: { value: '10' } });
-    expect(numberInput).toHaveValue(10);
+    const expectedNumber = 10;
+    fireEvent.change(numberInput, { target: { value: expectedNumber.toString() } });
+    expect(numberInput).toHaveValue(expectedNumber);
   });
 
   test('setCurrentNOE is called when the input changes', () => {
     const numberInput = screen.getByRole('spinbutton');
-    fireEvent.change(numberInput, { target: { value: '10' } });
-    expect(setCurrentNOE).toHaveBeenCalledWith(10);
+    const expectedNumber = 10;
+    fireEvent.change(numberInput, { target: { value: expectedNumber.toString() } });
+    expect(setCurrentNOE).toHaveBeenCalledWith(expectedNumber);
   });
 });
